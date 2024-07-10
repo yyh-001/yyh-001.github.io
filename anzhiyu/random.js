1a55e12bc7ff7eae865a3b45b47f7f85e85e789c
yyh <1807875605@qq.com>
2024-07-10 11:09:26 +0800

    Site updated: 2024-07-10 11:09:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/10/hello-md/","2024/07/10/hello-world/"];function toRandomPost(){
+var posts=["2024/07/10/hello-md/","2024/07/10/开发文档/","2024/07/10/【jetson笔记】ubuntu设置代理解决访问github问题/","2024/07/10/1000题/","2024/07/10/高数/","2024/07/10/飞机预警模型/","2024/07/10/敏感词软件使用说明/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
